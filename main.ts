a0ede7ba0f4237c1bfe9f7ea827e2895d7648b37
KhalidGhani333 <159691720+KhalidGhani333@users.noreply.github.com>
2024-02-27 15:14:46 +0500

    Calculator

---
+++ main.ts
@@ -0,0 +1,50 @@
+import inquirer from "inquirer";
+
+const Answer:{
+    "No1": number,
+    "No2": number,
+    "operator": string
+
+} = await inquirer.prompt([{
+
+type: "number",
+name: "No1",
+message: "write your No1"
+},
+{
+
+    type: "number",
+    name: "No2",
+    message: "write your No2"
+    },
+    {
+    type: "list",
+    name: "operator",
+    message: "select your operator",
+    choices: ["+","-","*","/"]
+
+}
+])
+
+let {No1,No2,operator}= Answer
+
+if( No1 && No2 && operator){
+    let result: number = 0;
+
+    if( operator == "+"){
+    result = No1 + No2}
+
+     else if( operator == "-")
+     {result = No1 - No2}
+
+     else if( operator == "*")
+     {result = No1 * No2}
+
+     else if( operator == "/")
+     {result = No1 / No2}
+
+     {console.log("Your Answer is: "+ result)}
+    
+}
+
+else{console.log("please select correct Operator")}
